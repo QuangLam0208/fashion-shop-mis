57eda0227a5dfd40ee1b0488a0fd4ec76e6147fb
Ngoc Manh <ngocmanhnctphysic@gmail.com>
2026-05-17 17:37:44 +0700

    fix controller

--- app/src/main/java/com/fashion/controller/CategoryController.java
+++ app/src/main/java/com/fashion/controller/CategoryController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import com.fashion.service.CategoryService;
 import java.util.List;
 
 @RestController
