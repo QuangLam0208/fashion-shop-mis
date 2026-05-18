aebbd3d6c0ce42232f918082126a873d9cedb600
Luong Quang Lam <laulua1223@gmail.com>
2026-05-18 10:26:50 +0700

    add: annotations

--- app/src/main/java/com/fashion/dto/request/CategoryRequestDTO.java
+++ app/src/main/java/com/fashion/dto/request/CategoryRequestDTO.java
@@ -1,8 +1,14 @@
 package com.fashion.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoryRequestDTO {
     private String name;
     private Long parentId;
