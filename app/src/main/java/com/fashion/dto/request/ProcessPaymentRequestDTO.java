c9cb8533a4fa9e52ce92b7d5c4c176a74d6f96c3
Luong Quang Lam <laulua1223@gmail.com>
2026-05-19 23:48:26 +0700

    add: process payment req

---
+++ app/src/main/java/com/fashion/dto/request/ProcessPaymentRequestDTO.java
@@ -0,0 +1,20 @@
+package com.fashion.dto.request;
+
+import com.fashion.model.enums.PaymentMethod;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProcessPaymentRequestDTO {
+    @NotNull(message = "ID đơn hàng không được để trống")
+    private Long orderId;
+
+    @NotNull(message = "Phương thức thanh toán không được để trống")
+    private PaymentMethod paymentMethod;
+}
