e31d57811613070930ab84551c476d1c62ce8382
Ngoc Manh <ngocmanhnctphysic@gmail.com>
2026-05-28 18:32:04 +0700

    done task FAS47

--- app/src/main/java/com/fashion/service/category/CategoryServiceImpl.java
+++ app/src/main/java/com/fashion/service/category/CategoryServiceImpl.java
@@ -43,6 +43,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<CategoryResponseDTO> getCategoryTree() {
         return categoryRepository.findByParentIsNull()
                 .stream()
