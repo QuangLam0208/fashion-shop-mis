4324497dd234c78554da512d003e64292ad02740
Ngoc Manh <ngocmanhnctphysic@gmail.com>
2026-06-09 22:36:41 +0700

    done US28

--- app/src/main/java/com/fashion/repository/OrderHistoryRepository.java
+++ app/src/main/java/com/fashion/repository/OrderHistoryRepository.java
@@ -1,9 +1,13 @@
 package com.fashion.repository;
 
+import com.fashion.model.Order;
 import com.fashion.model.OrderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
+    // Lấy danh sách đơn hàng theo userId, sắp xếp mới nhất lên đầu
+    List<Order> findByUser_UserIdOrderByOrderDateDesc(Long userId);
 }
